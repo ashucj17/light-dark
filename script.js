e405260d3c68a634be187fcf69129fc99e176876
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-11-07 20:00:14 +0530

    typed js library

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let dayLight = document.querySelector(".dayNight")
-let banner = document.querySelector(".banner")
+let dayLight = document.querySelector(".dayNight");
+let banner = document.querySelector(".banner");
 
-dayLight.addEventListener('click',()=>{
-    banner.classList.toggle("night")
-})
+dayLight.addEventListener("click", () => {
+  banner.classList.toggle("night");
+});
 
+let typingEffect = new Typed("#agniDev", {
+  strings: ["AgniDev", "Frontend Developer", "Code Enthusiast"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  backDelay: 1500,
+  loop: true,
+});
